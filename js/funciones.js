0e3bbc55377e986b7f281c0a5582d6b89a375a72
SchatzM <bri@npass.us>
2020-06-08 16:16:44 +0200

    Añadida lógica JS

--- js/funciones.js
+++ js/funciones.js
@@ -1,1 +1,40 @@
+/**
+ * Práctica 13
+ * Cambio por Fecha
+ * Brian Passos
+ */
 'use strict';
+
+const	elemento = {
+			colorCambiante: document.body.querySelector ('.colorCambiante'),
+			clasesColores : ['domingo', 'lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado']
+		},
+		colorDía = (díaSemana) => {
+			let día = () => {
+					return Number (díaSemana != undefined ? díaSemana : new Date ().getDay ());
+				},
+				colorDelDía = elemento.clasesColores [día()],
+				cantidadClases = elemento.clasesColores.length-1,
+				elementosBienvenida = {
+					Título: elemento.colorCambiante.querySelector('h1'),
+					Subtítulo: elemento.colorCambiante.querySelector('h2 input')
+				};
+
+			if (día () > cantidadClases || día () < 0) {
+				return console.error ('día no válido');
+			}
+
+			elemento.colorCambiante.classList.remove (...elemento.clasesColores);
+			elemento.colorCambiante.classList.add (colorDelDía);
+
+			elementosBienvenida.Título.innerText = colorDelDía;
+			elementosBienvenida.Subtítulo.max = cantidadClases;
+			elementosBienvenida.Subtítulo.addEventListener ('change', () => {
+				setTimeout (() => {
+					colorDía (elementosBienvenida.Subtítulo.value)
+				}, 200);
+			});
+			elementosBienvenida.Subtítulo.value = día ();
+		};
+
+colorDía ();
